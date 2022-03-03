4e2d140a9cfe63b45d81f14c528bef05e9941180
deadA1R <ilya.rogov.2003@gmail.com>
2022-03-03 12:07:32 +0600

    Final Project

---
+++ script/jqscript.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+  $(".rule-1").click(function(){
+    $(".panel-1").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".rule-2").click(function(){
+    $(".panel-2").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".rule-3").click(function(){
+    $(".panel-3").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".rule-4").click(function(){
+    $(".panel-4").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".rule-5").click(function(){
+    $(".panel-5").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".rule-6").click(function(){
+    $(".panel-6").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".rec-1").click(function(){
+    $(".panel-7").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".rec-2").click(function(){
+    $(".panel-8").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".recom").click(function(){
+    $(".rec").show("slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".cls-btn-rec").click(function(){
+    $(".rec").hide("slow");
+  });
+});
--- script/script.js
+++ script/script.js
@@ -1,6 +1,3 @@
-//Sidebar
-const icons = document.querySelectorAll('.icon');
-
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
 }
@@ -9,13 +6,108 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
+$("#dark-mode, #dark-mode-sidebar").click(function(){
+    $("body, .form-control, input").toggleClass("dark-mode");
+    $("footer").toggleClass("dark-mode-footer");
+    $('header').toggleClass("dm-header");
+    $('nav').toggleClass("navbar-dark");
+    $('main').toggleClass("dm-main");
+    $(".wrapper").toggleClass("dm-wrapper");
+    $(".card, .card-body").toggleClass("dm-card");
+    $("#article h5, h3, .panel").toggleClass("dm-text");
+    $(".btn-light").toggleClass("btn-dark");
+    $(".modal-content").toggleClass("dm-md-content");
+});
+
+$("#registr-sidebar, #registr").click(function() {
+  $("#myModal").css("display","block",closeNav());
+});
+
+$("#button_signup").click(function(){
+    if ($("#inputLogin").val() == "" || $("#inputPassword").val() == "") {
+      $('#danger').css("visibility" ,'visible');
+      $("#danger p").text("Enter login or password");
+      setTimeout(function() {
+        $('#danger').css("visibility", "hidden")}, 2000);
+    }
+    else{
+      $("#inputLogin").val("");
+      $("#inputPassword").val("");
+      $('#success').css("visibility" ,'visible');
+      $("#success p").text("You have logged in");
+      setTimeout(function() {
+        $('#success').css("visibility", "hidden")}, 2000);
+      $("#myModal").css("display","none");
+      var audio_send = new Audio('sound/sound_send.wav')
+      audio_send.play();
+    }
+});
+
+$("#button_reg").click(function(){
+    if ($("#regLogin").val() == "" || $("#regPassword").val() == "" || $("#retRegPassword").val() == "" || $("#regEmail").val() == "") {
+      $('#danger').css("visibility" ,'visible');
+      $("#danger p").text("Fill in all the fields");
+      setTimeout(function() {
+        $('#danger').css("visibility", "hidden")}, 2000);
+    }
+    else if ($("#regPassword").val() != $("#retRegPassword").val()) {
+      $('#danger').css("visibility" ,'visible');
+      $("#danger p").text("The repeated password was entered incorrectly.");
+      setTimeout(function() {
+        $('#danger').css("visibility", "hidden")}, 2000);
+    }
+    else{
+      $("#regLogin").val("");
+      $("#regEmail").val("");
+      $("#regPassword").val("");
+      $("#retRegPassword").val("");
+      $('#success').css("visibility" ,'visible');
+      $("#success p").text("Registration was successful!!!");
+      setTimeout(function() {
+        $('#success').css("visibility", "hidden")}, 2000);
+      $("#myModal").css("display","none");
+      var audio_send = new Audio('sound/sound_send.wav')
+      audio_send.play();
+    }
+});
+
+$("#button-toRegistr").click(function() {
+  $(".sign-up").css("display","none");
+  $(".form-registration").css("display","block");
+});
+
+$("#button-Auth").click(function() {
+  $(".form-registration").css("display","none");
+  $(".sign-up").css("display","block");
+});
+
+
+
+var span = document.getElementsByClassName("close")[0];
+var modal = document.getElementById("myModal");
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(ev) {
+  if (ev.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+//modal contenr
+
+//Sidebar
+const icons = document.querySelectorAll('.icon');
+
 let i = 0;
 
 function clickNavBtn() {
   icons.forEach (icon => {
     icon.addEventListener('click', () => {
       icon.classList.toggle("open");
-      if (i==0){
+      if (i == 0){
         openNav();
         i = 1;
       }
@@ -23,7 +115,6 @@ function clickNavBtn() {
         closeNav();
         i = 0;
       }
-
   });
   });
 }
@@ -84,9 +175,6 @@ document.addEventListener('keypress', (e) => {
       break;
     case "&":
       document.location.href = "index_movie.html";
-      break;
-    case "*":
-      document.location.href = "index_creators.html";
       break;
   }
 });
